e4fc4861952c614b19dda5b37987a47fbd890884
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-09-07 12:08:08 +0430

    Course belongs_to user relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
+User.create!(email: "boz@gmail.com", password: "kokolak", password_confirmation: "kokolak")
+
+puts "User created"
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
     }])
 end
+
+puts "30 Coureses created"
